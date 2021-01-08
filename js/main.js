88db8e6fb00653aa379adefbbcde789df9b7fabc
Federico Moretti <hello@federicomoretti.it>
2021-01-08 14:18:17 +0100

    update JavaScript and JSDoc

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 const instructions = document.getElementById('instructions');
-const hideInstructions = () => {
-
-};
 
 /**
- * Close the instructions modal on user input.
+ * Hide the instructions modal on page loading, if a user has already read it.
+ */
+const hideInstructions = () => {
+  if (localStorage.getItem('instructions')) {
+    instructions.style.display = 'none';
+  }
+};
+window.onload = hideInstructions;
+
+/**
+ * Close the instructions modal on a user input.
  */
 const closeInstructions = () => {
   instructions.style.opacity = '0';
   instructions.style.visibility = 'hidden';
+
+  /**
+   * Remember that a user has already read the instructions.
+   */
+  if (typeof(Storage) !== 'undefined') {
+    localeStorage.setItem('instructions', 'read');
+  }
 };
 const closeInstructionsButton = document.getElementById('close-instructions');
 closeInstructionsButton.addEventListener('click', closeInstructions, false);
